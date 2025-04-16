6c26c33f8c1be3e466511f00db22548ed09227ee
Thomas Pendleton III <105595323+Tpenny68@users.noreply.github.com>
2025-04-16 16:52:40 -0400

    dgvsjj

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -2,7 +2,7 @@ import java.util.Objects;
 
 /**
  * This class defines how power pills behave in 
- * a game.  The purpose of a power pill is to simply
+ * a game. The purpose of a power pill is to simply
  * provide a character with power.
  *
  * @author (You)
@@ -11,15 +11,16 @@ import java.util.Objects;
 public class PowerPill
 {
     private static final int DEFAULT_POWER = 10;
-    
+
     // instance variables
     private int powerSupply;
     private String name;
-    
+
     // constructors
+
     /*
      * Initializes this power pill to a default power value and
-     * and sets the name of the pill to name.
+     * sets the name of the pill to name.
      * @param name name of this power pill.
      */
     public PowerPill(String name)
@@ -27,10 +28,10 @@ public class PowerPill
         setName(name);
         setPower(DEFAULT_POWER);
     }
-    
+
     /*
-     * Initializes this power pill to the value of power  and
-     * and sets the name of the pill to name.
+     * Initializes this power pill to the value of power and
+     * sets the name of the pill to name.
      * @param name name of this power pill
      * @param power power level of this power pill.
      */
@@ -39,9 +40,9 @@ public class PowerPill
         setName(name);
         setPower(power);
     }
-    
+
     // accessor methods
-    
+
     /*
      * Returns the power provided by this pill.
      * @return the power provided by this power pill
@@ -50,7 +51,7 @@ public class PowerPill
     {
         return powerSupply;
     }
-    
+
     /*
      * Returns the name of this power pill.
      * @return the name of this power pill
@@ -59,18 +60,18 @@ public class PowerPill
     {
         return name;
     }
-    
+
     // mutator methods
-    
+
     /*
      * Sets the power value of this power pill.
      * @param power value to set for this power pill.
      */
     public void setPower(int power)
     {
-        powerSupply = power;
+        this.powerSupply = power;
     }
-    
+
     /*
      * Set the name of this power pill.
      * @param name the name to give this power pill.
@@ -80,4 +81,24 @@ public class PowerPill
         this.name = name;
     }
 
+    // toString method for printing
+    @Override
+    public String toString() {
+        return "PowerPill{name='" + name + "', power=" + powerSupply + "}";
+    }
+
+    // equals method for comparing pill objects
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PowerPill powerPill = (PowerPill) o;
+        return powerSupply == powerPill.powerSupply && Objects.equals(name, powerPill.name);
+    }
+
+    // hashCode method to go with equals
+    @Override
+    public int hashCode() {
+        return Objects.hash(powerSupply, name);
+    }
 }
